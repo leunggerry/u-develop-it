2e4ed46ba275db7686aad1dc2ac13f2a61d61dfa
gigi.upNorth <leung.gerry@gmail.com>
2022-08-10 18:31:09 -0400

    [SQL node] adding sql call to fetch rows form candidates

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+//Requried modules
 const express = require("express");
+const mysql = require("mysql2");
 
 // Create port number and app expression
 const PORT = process.env.PORT || 3001;
@@ -7,6 +9,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to a database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // your MySQL username,
+    user: "root",
+    // your MySQL password
+    password: "Gigi22)*",
+    database: "election",
+  },
+  console.log("connected to the election database.")
+);
+
+db.query("SELECT * FROM candidates", (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
